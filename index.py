b00f06d9ac0073c04f49c6c575c33f79c1073149
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-03-16 14:45:06 +0000

    CORS issue

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify, request
+from flask_cors import CORS
 import json
 import requests 
 
 app = Flask(__name__)
+CORS(app)
 def getArriaContent():
     null = None
     url = 'https://app.studio.arria.com:443/alite_content_generation_webapp/text/v3g3XPEvkxw'
