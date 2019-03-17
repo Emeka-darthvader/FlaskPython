1d8cf921fb2fa678eb253b99014928d9abf9ebd8
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-03-17 09:21:24 +0000

    test 1

--- getVisualizations.py
+++ getVisualizations.py
@@ -9,8 +9,19 @@ import fix_yahoo_finance as yf
 
 yf.pdr_override()
 
-data = pdr.get_data_yahoo("SPY", start="2018-09-12", end="2019-03-16")
 
-#print(data)
-#df.iloc[:, [True, False, True, False]]
-print(data['Adj Close'][0])
+def getViz():
+    allViz = []
+    data = pdr.get_data_yahoo("SPY", start="2018-09-12", end="2019-03-16")
+
+    #print(data)
+    #df.iloc[:, [True, False, True, False]]
+    viz= (data['Adj Close'])
+    length = data['Adj Close'].count()
+    for i in range(0,length):
+        allViz.append(viz)
+    return allViz
+
+ok=getViz()
+
+print(ok)
--- index.py
+++ index.py
@@ -16,15 +16,17 @@ app = Flask(__name__)
 CORS(app)
 
 def getVisualizations():
+    
     allViz = []
     data = pdr.get_data_yahoo("SPY", start="2018-09-12", end="2019-03-16")
-    
+
     #print(data)
     #df.iloc[:, [True, False, True, False]]
-    VizInput = data['Adj Close']
-    #length = data.shape['Adj Close'][0]
-    #for i in range(0,)
-    return VizInput
+    viz= (data['Adj Close'])
+    length = data['Adj Close'].count()
+    for i in range(0,length):
+        allViz.append(viz)
+    return allViz
 
 def getArriaContent():
     null = None
